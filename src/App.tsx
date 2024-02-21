54bbcc7018726567db3849da0cc7f3e2bf98d660
Your Name <rimonron.ad@gmail.com>
2024-02-21 12:23:55 +0600

    Payload and TypeSafe Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { useDispatch, useSelector } from "react-redux"
-import { decrement, increment } from "./redux/features/CounterSlice"
+import { decrement, increment, incrementByFive } from "./redux/features/CounterSlice"
+import { useAppDispatch, useAppSelector } from "./redux/hooks"
 
 
 function App() {
-  const {count} = useSelector((state) => state.counter)
-  const dispatch = useDispatch()
+  const {count} = useAppSelector((state) => state.counter)
+  const dispatch = useAppDispatch()
 
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <div className="flex border border-purple-300 rounded-md bg-slate-50 p-10">
-        <button onClick={() => dispatch(increment())} className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">Increment</button>
+        <button onClick={() => dispatch(incrementByFive(5))} className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">Increment  By 5</button>
+        <button onClick={() => dispatch(increment())} className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white mx-2">Increment</button>
         <h1 className="text-3xl mx-10">{count}</h1>
         <button onClick={() => dispatch(decrement())} className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">Decrement</button>
       </div>
--- src/redux/features/CounterSlice.tsx
+++ src/redux/features/CounterSlice.tsx
@@ -1,21 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from '@reduxjs/toolkit'
 
-const initialState = {count: 0}
+type CounterType = {
+    count: number;
+}
+
+const initialState : CounterType = {count: 0}
 
 const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
-        increment: (state, action) =>{
+        increment: (state) =>{
             state.count = state.count + 1
         },
-        decrement: (state, action) =>{
+        decrement: (state) =>{
             state.count = state.count - 1
+        },
+        incrementByFive: (state, action: PayloadAction<number>) =>{
+            state.count = state.count + action.payload
         }
     }
 
 })
 
-export const {increment, decrement} = counterSlice.actions
+export const {increment, decrement, incrementByFive} = counterSlice.actions
 
 export default counterSlice.reducer
